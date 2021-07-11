88eaeb262929c3d9f2e80f1db8fc7591d158a383
Dominik Nagy <dominiknagy@pop-os.localdomain>
2021-07-11 20:55:15 +0200

    What the fuck?

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,7 +5,7 @@ import test.Yo;
 public class HelloWorld {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
-        app.get("/", ctx -> ctx.result("Hello World4"));
+        app.get("/", ctx -> ctx.result("wtf"));
 
         System.out.println(Yo.getYo());
     }
